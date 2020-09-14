ab56d2cfd1d51205c42cb081254c93438b1fb7e6
Mustafa <mmustafaadn@gmail.com>
2020-09-15 01:14:08 +0200

    initialize the project

---
+++ project/app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask, make_response
+from dotenv import load_dotenv
+
+def create_app():
+    """ create the app instance """
+	
+    # load the variables from the env file!
+	load_dotenv()
+
+	from . import models, api_views
+	app = Flask(__name__)
+
+	# Configurations
+	app.config.from_object('config')
+	
+	models.db.init_app(app)
+
+	# register the blueprints
+	app.register_blueprint(api_views.api, url_prefix='/api')
+	
+	return app
